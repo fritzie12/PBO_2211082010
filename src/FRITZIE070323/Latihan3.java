3ad03b6bd4348c809c8054b8357c9532ef6366e6
fritzie12 <fritzieyustini@gmail.com>
2023-03-08 11:19:27 +0700

    Initial commit

---
+++ src/FRITZIE070323/Latihan3.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FRITZIE070323;
+
+/**
+ *
+ * @author LABSIKOMP03
+ */
+public class Latihan3 {
+    public static void main (String[]args){
+        int a = 10;
+        int b = 23;
+        int c = 5;
+        int max;
+        
+        max = (a>=b)? a:b;
+        max = (c>=max)? c:max;
+        
+        
+        System.out.println("number 1 = "+a);
+        System.out.println("number 2 = "+b);
+        System.out.println("number 3 = "+c);
+        System.out.println("nilai maksimal = "+max);
+        
+        
+        
+        
+    }
+}
