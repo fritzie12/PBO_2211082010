67ed3bd2d1a7ebe9588c5af416223b1f58437906
fritzie12 <fritzieyustini@gmail.com>
2023-04-10 11:06:20 +0700

    baru

---
+++ src/FRITZIE080423/Kuda.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package FRITZIE080423;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Kuda extends LivingThing{
+    public void berjalan (){
+        System.out.println("Kuda berjalan");
+    }
+}
